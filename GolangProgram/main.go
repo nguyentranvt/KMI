12ddea9019d95c76ee5fdf7a9719e7fe174a0329
nguyentranvt <nguyentranvt4594@gmail.com>
2022-09-19 21:52:25 +0900

    add new

--- GolangProgram/main.go
+++ GolangProgram/main.go
@@ -4,8 +4,15 @@ func main() {
 	result := AddAtoB(1, 2)
 	//print the result
 	println(result)
+	ResultNewValue := PrintHelloWorld()
+	println(ResultNewValue)
 }
 
 func AddAtoB(ANumber int, BNumber int) int {
 	return ANumber + BNumber
+}
+
+func PrintHelloWorld() string {
+	return "Hello Cac em"
+
 }
