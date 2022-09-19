24865233c3d050bda4d57c5fde9fb44939d635da
nhatkhoi <vnkhoi1992@gmail.com>
2022-09-19 19:51:30 +0700

    helloworld

--- GolangProgram/main.go
+++ GolangProgram/main.go
@@ -4,11 +4,13 @@ func main() {
 	result := AddAtoB(1, 2)
 	//print the result
 	println(result)
+	ResultNewValue := PrintHelloWorld()
+	println(ResultNewValue)
 }
 
 func AddAtoB(ANumber int, BNumber int) int {
 	return ANumber + BNumber
 }
 func PrintHelloWorld()string{
-	return "Hello World"
+	return "Hello World Viet Nam"
 } 
