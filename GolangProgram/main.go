540d7e397269948d75c5651e0fd52ab62b298519
Le Huu Duy <lehuuduy351991@gmail.com>
2022-09-23 23:54:08 +0900

    cập nhật

--- GolangProgram/main.go
+++ GolangProgram/main.go
@@ -1,9 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	result := AddAtoB(1, 2)
+	//result := AddAtoB(1, 2)
 	//print the result
-	println(result)
+	//println(result)
+
+	//InputAddress
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your address: ")
+	address, _ := reader.ReadString('\n')
+	//InputPhoneNumber
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your phone number: ")
+	phonenumber, _ := reader.ReadString('\n')
+	//GrilFriend
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your gril friend number:")
+	grilfriend, _ := reader.ReadString('\n')
+
+	fmt.Print("You live in: " + address)
+	fmt.Print("Your phone number: " + phonenumber)
+	fmt.Print("Your current number of girlfriends:  " + grilfriend)
 }
 
 func AddAtoB(ANumber int, BNumber int) int {
